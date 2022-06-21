2909567c0e677305789ec2c5ec80fb3083681312
Andree Panjaitan <panjaitanandree@gmail.com>
2022-06-21 23:49:23 +0700

    update

--- src/components/organisms/Navbar/index.jsx
+++ src/components/organisms/Navbar/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import Constant from "../../../constants";
@@ -17,70 +17,89 @@ import { useState } from "react";
 function Navbar({ navbar, isTransparent, ...props }) {
   const [key, setKey] = useState("");
   const navigate = useNavigate();
+  const [bgWhite, setBgWhite] = useState(false);
   const location = useLocation();
-  return (
-    <NavbarB
-      collapseOnSelect
-      expand="lg"
-      className={buildClass({
-        "shadow-sm bg-white": !(location.pathname === "/"),
-        absolute: location.pathname === "/",
-      })}
-    >
-      <Container fluid>
-        <Link to={"/"} className="navbar-brand">
-          <div className="navbar-icon rounded-circle">
-            <img src={Constant.LOGO} alt="" />
-          </div>
-        </Link>
-        <NavbarB.Toggle aria-controls="basic-navbar-nav" />
+  useEffect(() => {
+    const handleWindowScroll = (event) => {
+      if (event.target.documentElement.scrollTop > 200) {
+        setBgWhite(true);
+      } else {
+        setBgWhite(false);
+      }
+    };
+    window.addEventListener("scroll", handleWindowScroll, false);
+    return () => {
+      window.removeEventListener("scroll", handleWindowScroll);
+    };
+  }, [key, bgWhite]);
 
-        <NavbarB.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto mt-md-0 mt-3 d-flex gap-3">
-            {navbar.map((item, index) => (
-              <Link
-                key={index}
-                to={item.link}
-                className={buildClass({
-                  "navbar-link navbar-top-link": true,
-                  active: location.pathname === item.link,
-                })}
+  return (
+    <div>
+      <NavbarB
+        collapseOnSelect
+        expand="lg"
+        fixed="top"
+        className={buildClass({
+          "shadow-sm bg-white": !(location.pathname === "/"),
+          "bg-white": location.pathname === "/" && bgWhite,
+        })}
+      >
+        <Container fluid>
+          <Link to={"/"} className="navbar-brand">
+            <div className="navbar-icon rounded-circle">
+              <img src={Constant.LOGO} alt="" />
+            </div>
+          </Link>
+          <NavbarB.Toggle aria-controls="basic-navbar-nav" />
+
+          <NavbarB.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto mt-md-0 mt-3 d-flex gap-3">
+              {navbar.map((item, index) => (
+                <Link
+                  key={index}
+                  to={item.link}
+                  className={buildClass({
+                    "navbar-link navbar-top-link": true,
+                    active: location.pathname === item.link,
+                  })}
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </Nav>
+            <div className="d-flex ms-auto align-items-center">
+              <form
+                action=""
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  console.log(key);
+                  navigate(`/search/${key}`);
+                }}
               >
-                {item.name}
-              </Link>
-            ))}
-          </Nav>
-          <div className="d-flex ms-auto align-items-center">
-            <form
-              action=""
-              onSubmit={(event) => {
-                event.preventDefault();
-                console.log(key);
-                navigate(`/search/${key}`);
-              }}
-            >
-              <div className="d-flex align-items-center">
-                <div className="form-group">
-                  <input
-                    className="form-control"
-                    onChange={(event) => {
-                      setKey(event.currentTarget.value);
-                    }}
-                    type="text"
-                    name=""
-                    value={key}
-                    id=""
-                  />
+                <div className="d-flex align-items-center">
+                  <div className="form-group">
+                    <input
+                      className="form-control"
+                      onChange={(event) => {
+                        setKey(event.currentTarget.value);
+                      }}
+                      type="text"
+                      name=""
+                      value={key}
+                      id=""
+                    />
+                  </div>
+                  <button className="btn btn-success" type="submit">
+                    <FontAwesomeIcon icon={"search"} />
+                  </button>
                 </div>
-                <button className="btn btn-success" type="submit">
-                  <FontAwesomeIcon icon={"search"} />
-                </button>
-              </div>
-            </form>
-          </div>
-        </NavbarB.Collapse>
-      </Container>
-    </NavbarB>
+              </form>
+            </div>
+          </NavbarB.Collapse>
+        </Container>
+      </NavbarB>
+      <div style={{ height: "75px" }}></div>
+    </div>
   );
 }
 
